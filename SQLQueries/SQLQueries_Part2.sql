4a8cfd3cec562f10afd799232053ace7ef77f380
Jeison E Giraldo <jeisonegiraldo@hotmail.com>
2022-03-07 21:52:07 -0500

    Update SQLQueries_Part2.sql

--- SQLQueries/SQLQueries_Part2.sql
+++ SQLQueries/SQLQueries_Part2.sql
@@ -24,3 +24,30 @@ where "2018[yr2018]" = (select avg("2018[yr2018]") from climate_change);
 select "countrycode", "2018"
 from emisiones_co2
 where "2018" = (select min ("2018") from emisiones_co2);
+
+
+--Consultas Climate Change
+SELECT * FROM "ccdb"."climate_change";
+
+--Total países en el dataset
+SELECT COUNT (DISTINCT "country name")
+FROM  "ccdb"."climate_change";
+
+--Total indicadores en el dataset
+SELECT COUNT (DISTINCT "series name")
+FROM  "ccdb"."climate_change";
+
+--la temperaturas promedio del 2000
+SELECT COUNT("2000 [YR2000]") as "Promedio emisiones CO2 2000" 
+FROM "ccdb"."climate_change";
+
+--los indicadores en 2010 con su máximo valor tomado
+SELECT "series name", MAX("2000 [YR2000]") as "Máximo valor indicador"
+FROM "ccdb"."climate_change"
+GROUP BY "series name"
+ORDER BY "Máximo valor indicador" desc
+LIMIT 10;
+
+--los países en 2000 con el máximo valor tomado
+SELECT "country name", MAX("2000 [YR2000]") as "Máximo valor registrado 2000"
+FROM "ccdb"."climate_change"
